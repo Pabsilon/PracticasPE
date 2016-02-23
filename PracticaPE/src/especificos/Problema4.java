624563d75235e21ea0e4e2cb47b8b4cfb69636fb
Pabs <pablomacveigh@gmail.com>
2016-02-23 17:48:11 +0100

    Intento de implementación de Problema4. Aun no da los valores esperados.

--- PracticaPE/src/especificos/Problema4.java
+++ PracticaPE/src/especificos/Problema4.java
@@ -3,17 +3,40 @@ import implementacion.*;
 
 public class Problema4 extends Cromosoma{
 
-	//TODO
+	private float _xmax = (float) Math.PI;
+	private float _xmin = 0;
+	private int _n;
+	
+	public Problema4(float precision, int n){
+		_n =n;
+		_longitud = new int[_n];
+		
+		for (int i =0; i<_n; i++){
+			_longitud[i] = (int) Math.ceil(((Math.log(1+(_xmax-_xmin)/precision))/Math.log(2)));
+		}
+		_genes = new Gen[_n];
+		for (int i = 0; i<_n; i++){
+			_genes[i]=new Gen(_longitud[i]);
+		}
+	}
 	
 	protected float[] getFenotipo() {
-		//TODO
-		return new float[0];
+		float[] retval = new float[_n];
+		for (int i = 0; i<_n; i++){
+			retval[i] = (float) (_xmin + (_xmax - _xmin) * bin_dec(_genes[i]) / (Math.pow(2, _longitud[i])-1));		
+		}
+		return retval;
 	}
 
-	@Override
 	protected float getAptitud() {
-		//TODO
-		return 0;
+		//TODO CORRECT
+		float retval = 0;
+		//f(xi|i=1..n) = -Sum (from i=1 to n) sen(xi) * sen²⁰((i+1)*xi²)/π);
+		for (int i = 0; i<_n; i++){
+			float x = getFenotipo()[i];
+			retval+= -Math.sin(x)*Math.pow(20, (Math.sin((i+2)*Math.pow(2, x))/Math.PI));
+		}
+		return retval;
 	}
 
 }
--- PracticaPE/src/implementacion/Main.java
+++ PracticaPE/src/implementacion/Main.java
@@ -6,10 +6,8 @@ public class Main {
 	public static void main(String[] args) {
 		Cromosoma crom[] = new Cromosoma[10];
 		for (int i = 0; i<10; i++){
-			crom[i]=new Problema5(0.01f);
+			crom[i]=new Problema4(0.01f,1);
 			System.out.print(crom[i].getFenotipo()[0]);
-			System.out.print(" ");
-			System.out.print(crom[i].getFenotipo()[1]);
 			System.out.print(" ");
 			System.out.println(crom[i].getAptitud());
 		}
