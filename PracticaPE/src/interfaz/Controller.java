b74fdbf1313d5e6ed52b6e35bf8e05e43d6c9ff1
Pabs <pablomacveigh@gmail.com>
2016-02-29 13:07:25 +0100

    n=1 para el problema 4

--- PracticaPE/src/interfaz/Controller.java
+++ PracticaPE/src/interfaz/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
 	void comenzarSimulacion(String precision, String cruce, String mutacion, String metodoSelec, String problema)
 	{
 		//TODO: El último parámetro de la función es la n para el problema 4. De momento está a 0, pero hay que parsearlo desde la interfaz.
-		Algoritmo ag = new Algoritmo(100, Float.parseFloat(precision), Float.parseFloat(cruce) / 100, Float.parseFloat(mutacion) / 100, metodoSelec, problema, 100, 0);
+		Algoritmo ag = new Algoritmo(100, Float.parseFloat(precision), Float.parseFloat(cruce) / 100, Float.parseFloat(mutacion) / 100, metodoSelec, problema, 100, 1);
 		
 		double[] mejorAbsoluto = new double[100];
 		double[] mejorGeneracion = new double[100];
