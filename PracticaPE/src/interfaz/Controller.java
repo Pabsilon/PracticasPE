fd122d6d5d23063f6701fabad57ac5bc7672296c
Pabs <pablomacveigh@gmail.com>
2016-03-02 03:20:50 +0100

    Actualización de comentarios.

--- PracticaPE/src/interfaz/Controller.java
+++ PracticaPE/src/interfaz/Controller.java
@@ -12,15 +12,14 @@ public class Controller {
 	
 	void comenzarSimulacion(String precision, String cruce, String mutacion, String metodoSelec, String problema, String numPoblacion, String numGeneraciones, String semilla, String n, String participantes)
 	{
-		//TODO: El último parámetro de la función es la n para el problema 4. De momento está a 0, pero hay que parsearlo desde la interfaz.
-		//Crear un nuevo algoritmo
+		//Parseo y construccion del nuevo algoritmo.
 		Algoritmo ag = new Algoritmo(Integer.parseInt(numPoblacion), Float.parseFloat(precision), Float.parseFloat(cruce) / 100, Float.parseFloat(mutacion) / 100, metodoSelec, problema, Integer.parseInt(numGeneraciones), Long.parseLong(semilla), Integer.parseInt(n), Integer.parseInt(participantes));
 		
 		double[] mejorAbsoluto = new double[Integer.parseInt(numGeneraciones)];
 		double[] mejorGeneracion = new double[Integer.parseInt(numGeneraciones)];
 		double[] mediaGeneracion = new double[Integer.parseInt(numGeneraciones)];
 		
-		//Ejecutar el algoritmo
+		//Ejecución del algoritmo
 		String resultado;
 		resultado = ag.simular(mejorAbsoluto, mejorGeneracion, mediaGeneracion);
 		
