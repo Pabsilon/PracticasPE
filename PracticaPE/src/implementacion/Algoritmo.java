efdd99e21ec5a71947259cd54559251c8a78f99e
Pabs <pablomacveigh@gmail.com>
2016-03-02 03:19:51 +0100

    Añadidos comentarios.

--- PracticaPE/src/implementacion/Algoritmo.java
+++ PracticaPE/src/implementacion/Algoritmo.java
@@ -21,6 +21,7 @@ public class Algoritmo {
 	
 	public Algoritmo(int poblacion, float precision, float cruce, float mutacion, String metodoSelec, String problema, int simulaciones, long semilla, int n, int participantes) 
 	{
+		//Seteo de los atributos necesarios
 		_poblacionTamano = poblacion;
 		_precision = precision;
 		_cruceProb = cruce;
@@ -31,23 +32,30 @@ public class Algoritmo {
 		_rand = new Random();
 		_n = n;
 		_participantes = participantes;
+		
+		//Tratamiento del random. 0 es una semilla random, otros valores son semillas a pincho.
 		if (semilla !=0){
 			_rand.setSeed(semilla);
 		}else{
 			_rand = new Random();
 		}
 		
+		//Inicialización de la poblacion.
 		_poblacion = new Cromosoma[poblacion];
 		for(int i = 0; i < poblacion; i++)
 		{
+			//Llevamos un parámetro de mas sobretodo para el problema 4.
 			_poblacion[i] = ProblemaFabrica.getCromosomaProblema(problema, precision, n, _rand);
 		}
 		
+		//El mejor individuo de la primera generación lo elegimos aleatoriamente
+		//Esto es solo para representar correctamente la grafica.
 		_mejorIndividuo = ProblemaFabrica.getCromosomaProblema(problema, precision, n, _rand);
 	}
 	
 	public String simular(double[] mejorAbsoluto, double[] mejorGeneracion, double[] mediaGeneracion)
 	{
+		//Variables para guardar los resultados obtenidos
 		float[] aptitudes = new float[_poblacionTamano];
 		float[] puntuaciones = new float[_poblacionTamano];
 		float[] puntuacionesAcum = new float[_poblacionTamano];
@@ -55,30 +63,33 @@ public class Algoritmo {
 		
 		for(int i = 0; i < _simulaciones; i++)
 		{
-			//Evaluar
+			//Se evaluan todos los valores de la población
 			double[] infoGeneracion = new double[2]; //0 Mejor de la generacion, 1 Media de la generacion
 			evaluar(aptitudes, puntuaciones, puntuacionesAcum, infoGeneracion,_poblacion[0].isMaximizing());
 			mejorAbsoluto[i] = _mejorValor;
 			mejorGeneracion[i] = infoGeneracion[0];
 			mediaGeneracion[i] = infoGeneracion[1];
 			
-			//Seleccionar
+			//Seleccionamos para el cruce
 			seleccionar(aptitudes, puntuacionesAcum);
 			
-			//Cruzar
+			//Cruzamos
 			cruzar();
 			
-			//Mutar
+			//Mutamos
 			mutar();
 		}
 		
+		//Salida del algoritmo
 		if (!_problema.equals("Problema4")){
+			//Problemas 1-3 y 5
 			 resultado = "Valor mejor: " + _mejorValor + " en X: " + _mejorIndividuo.getFenotipo()[0];
 			if (_mejorIndividuo.getFenotipo()[1]!=new Float("0")){
 				resultado = resultado + " Y: " + _mejorIndividuo.getFenotipo()[1];
 				
 			}
 		}else{
+			//Problema 4
 			resultado = "Valor mejor: " + _mejorValor;
 			for (int i=0; i<_n; i++){
 				resultado = resultado + ", x" + i +" = " + _mejorIndividuo.getFenotipo()[i];
@@ -89,7 +100,7 @@ public class Algoritmo {
 	
 	protected void evaluar(float[] aptitudes, float[] puntuaciones, float[] puntuacionesAcum, double[] infoGeneracion, boolean minMax)
 	{
-		//Maximizacion
+		//En caso de que sea un problema de maximización.
 		if (minMax){
 			float mejorAptitudEnGeneracion = Float.MIN_VALUE;
 			int mejorCromGeneracion = 0;
@@ -101,7 +112,7 @@ public class Algoritmo {
 				aptitudes[i] = _poblacion[i].getAptitud();
 				sumaAptitudes += aptitudes[i];
 				
-				//Actualizar el mejor de la generacion
+				//Actualizamos el mejor de la generacion
 				if(aptitudes[i] > mejorAptitudEnGeneracion)
 				{
 					mejorAptitudEnGeneracion = aptitudes[i];
@@ -125,10 +136,11 @@ public class Algoritmo {
 				puntuacionesAcum[i] = puntuaciones[i] + puntuacionesAcum[i - 1];
 			}
 			
-			//Informacion de la generacion (M�ximo y media)
+			//Informacion de la generacion (Maximo y media)
 			infoGeneracion[0] = mejorAptitudEnGeneracion;
 			infoGeneracion[1] = sumaAptitudes / _poblacionTamano;
 		}else{
+			//Si es un problema de minimización
 			float mejorAptitudEnGeneracion = Float.MAX_VALUE;
 			int mejorCromGeneracion = 0;
 			
@@ -165,7 +177,7 @@ public class Algoritmo {
 			}
 			puntuacionesAcum[_poblacionTamano - 1] = 1; //Asegurarse que el ultimo valor de las puntuaciones es 1
 			
-			//Informacion de la generacion (M�ximo y media)
+			//Informacion de la generacion (Minimo y media)
 			infoGeneracion[0] = mejorAptitudEnGeneracion;
 			infoGeneracion[1] = sumaAptitudes / _poblacionTamano;
 		}
@@ -174,11 +186,11 @@ public class Algoritmo {
 	private void seleccionar(float[] aptitudes, float[] puntuacionesAcum)
 	{
 		Cromosoma[] seleccionados = new Cromosoma[_poblacionTamano];
-		
+		//Construimos el algoritmo de selección elegido
 		AlgoritmoSeleccion ASeleccion = AlgoritmoSeleccionFabrica.getAlgoritmoDeSeleccion(_metodoSeleccion, _participantes);
 		ASeleccion.seleccionar(aptitudes, puntuacionesAcum, seleccionados, _poblacionTamano, _poblacion, _mejorIndividuo.isMaximizing(), _rand);
 		
-		//Crear la nueva poblacion
+		//Creamos la nueva población
 		_poblacion = seleccionados;
 		
 	}
@@ -188,7 +200,7 @@ public class Algoritmo {
 		int numeroSeleccionados = 0;
 		int[] aCruzar = new int[_poblacionTamano];
 		
-		//Obtener los cromosomas a cruzar
+		//Obtenemos los cromosomas a cruzar
 		for(int i = 0; i < _poblacionTamano; i++)
 		{
 			if(_rand.nextFloat() < _cruceProb)
@@ -197,20 +209,20 @@ public class Algoritmo {
 				numeroSeleccionados++;
 			}
 		}
-		//Hacerlos pares
+		//Redondeo a par
 		if(numeroSeleccionados % 2 == 1)
 		{
 			numeroSeleccionados--;
 		}
 		
-		//Cruzarlos
+		//Cruzamos
 		for(int i = 0; i < (numeroSeleccionados / 2); i++)
 		{
 			Cromosoma hijo1 = ProblemaFabrica.getCromosomaProblema(_problema, _precision, _n, _rand);
 			Cromosoma hijo2 = ProblemaFabrica.getCromosomaProblema(_problema, _precision, _n, _rand);
 			Cromosoma.cruzar(_poblacion[aCruzar[i]], _poblacion[aCruzar[i + 1]], hijo1, hijo2, _rand);
 			
-			//Sustituir a los padres
+			//Sustituimos a los padres
 			_poblacion[aCruzar[i]] = hijo1;
 			_poblacion[aCruzar[i + 1]] = hijo2;
 		}
@@ -218,6 +230,7 @@ public class Algoritmo {
 	
 	private void mutar()
 	{
+		//Probamos la mutación.
 		for(int i = 0; i < _poblacionTamano; i++)
 		{
 				_poblacion[i].mutar(_mutacionProb, _rand);
