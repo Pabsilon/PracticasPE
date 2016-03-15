01ec8042d27b31534c5de00668ced36ec094cfe3
Usuario Local <Usuario Local>
2016-03-15 14:40:31 +0100

    Implementado el problema 4 (faltan los metodos de seleccion 'Simulated binary crossover'). Implementacion oscura

--- PracticaPE/src/implementacion/CromosomaReal.java
+++ PracticaPE/src/implementacion/CromosomaReal.java
@@ -20,16 +20,67 @@ public abstract class CromosomaReal extends Cromosoma{
 	 */
 	protected abstract float getAptitud();
 	
+	@Override
+	public int getLongitudTotal() {
+		return _genes.length;
+	}
+	
+	@Override
+	public void copiarCromosoma(Cromosoma cromosoma) {
+		CromosomaReal cromReal = (CromosomaReal)cromosoma;
+		if(_genes.length == cromReal._genes.length)
+		{
+			for(int i = 0; i < _genes.length; i++)
+			{
+				_genes[i] = cromReal._genes[i];
+			}
+		}
+	}
+	
+	
 	/** Cruza dos cromosomas.
 	 * 
-	 * @param padre1 El primer padre a cruzar
-	 * @param padre2 El segundo padre a cruzar
-	 * @param hijo1 El primer hijo generado
-	 * @param hijo2 El segundo hijo generado
+	 * @param padreReal1 El primer padre a cruzar
+	 * @param padreReal2 El segundo padre a cruzar
+	 * @param hijoReal1 El primer hijo generado
+	 * @param hijoReal2 El segundo hijo generado
 	 * @param rand La función random a utilizar (se pasa por uso de semillas)
 	 */
-	public static void cruzar (CromosomaReal padre1, CromosomaReal padre2, CromosomaReal hijo1, CromosomaReal hijo2, Random rand){
+	public static void cruzarReal(Cromosoma padre1, Cromosoma padre2, Cromosoma hijo1, Cromosoma hijo2, Random rand){
+		CromosomaReal padreReal1 = (CromosomaReal)padre1;
+		CromosomaReal padreReal2 = (CromosomaReal)padre2;
+		CromosomaReal hijoReal1 = (CromosomaReal)hijo1;
+		CromosomaReal hijoReal2 = (CromosomaReal)hijo2;
+		//Cruce externo
+		for(int i = 0; i < hijoReal1._genes.length; i++)
+		{
+			hijoReal1._genes[i] = padreReal1._genes[i];
+			hijoReal2._genes[i] = padreReal2._genes[i];
+		}
 		
+		int puntoDeCorte;
+		if(padreReal1.getLongitudTotal() == 1)
+		{
+			puntoDeCorte = 0;
+		}
+		else
+		{
+			puntoDeCorte = rand.nextInt(padreReal1.getLongitudTotal() - 1) + 1; //Genera un numero (0, longitudCromosoma)
+		}
+		
+		//Hijo1
+		//Copiar resto de genes al completo
+		for(int i = puntoDeCorte; i < padreReal2._genes.length; i++)
+		{
+			hijoReal1._genes[i] = padreReal2._genes[i];
+		}
+		
+		//Hijo2
+		//Copiar resto de genes al completo
+		for(int i = puntoDeCorte; i < padreReal1._genes.length; i++)
+		{
+			hijoReal2._genes[i] = padreReal1._genes[i];
+		}
 	}
 	
 	/**Método que muta a la poblacion. Recorre todos los genes y decide si alterarlos.
@@ -37,8 +88,17 @@ public abstract class CromosomaReal extends Cromosoma{
 	 * @param mutacionProb Probabilidad de que mute un gen
 	 * @param rand Función de random a utilizar (se pasa por uso de semillas)
 	 */
-	public void mutar(float mutacionProb, Random rand){
-		
+	public void mutar(float mutacionProb, Random rand)
+	{
+		//Recorrer todos los alelos de los genes y mutar si procede
+			for(int i = 0; i < _genes.length; i++)
+			{
+				if(rand.nextFloat() < mutacionProb)
+				{
+					float aleatorio = rand.nextFloat() + 0.1f; //[0.1, 1.1]
+					_genes[i] = (_genes[i] + aleatorio) % (float)Math.PI; //Trampa: Sabemos que el dominio de los genes del problame 4 (cromosoma real) es PI
+				}
+			}
 	}
 	
 	
