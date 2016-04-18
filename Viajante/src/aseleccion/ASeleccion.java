4326f30bb7acf43d6a3c00da6972d4616d7623f5
Pabs <pablomacveigh@gmail.com>
2016-04-18 22:55:27 +0200

    Añadido un documento que parece interesante para la implementación de los cruces.

--- Viajante/src/aseleccion/ASeleccion.java
+++ Viajante/src/aseleccion/ASeleccion.java
@@ -2,6 +2,9 @@ package aseleccion;
 
 import implementation.Cromosoma;
 
+//http://www.dca.fee.unicamp.br/~gomide/courses/EA072/artigos/Genetic_Algorithm_TSPR_eview_Larranaga_1999.pdf
+//Trabajo que contiene bastante de lo que hacemos.
+
 public interface ASeleccion
 {
 	public void seleccionar(Cromosoma[] poblacion, float[] aptitudes, Cromosoma[] seleccionados);
