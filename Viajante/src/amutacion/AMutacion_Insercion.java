1b1fc93963ae9ce36f1b0b6ebb577029489b849d
Pabs <pablomacveigh@gmail.com>
2016-04-19 14:46:01 +0200

    Mutacion por insercion

--- Viajante/src/amutacion/AMutacion_Insercion.java
+++ Viajante/src/amutacion/AMutacion_Insercion.java
@@ -1,14 +1,28 @@
 package amutacion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 import implementation.Cromosoma;
 
 public class AMutacion_Insercion implements AMutacion {
 
-	@Override
+	//Se elije un gen al azar y se inserta en una posición al azar, moviendo el resto acordemente.
+	
 	public void mutar(Cromosoma[] poblacion, float mutacionProb, Random rand) {
-		// TODO Auto-generated method stub
+		
+		for (int i=0; i<poblacion.length; i++){
+			@SuppressWarnings("unchecked")
+			int alelos[] = poblacion[i].getGenotipo();
+			for (int j = 1; j<alelos.length; j++){
+				if (rand.nextFloat()<mutacionProb){
+					
+				}
+			}
+			poblacion[i].setGenList(alelos);
+		}
 		
 	}
 
