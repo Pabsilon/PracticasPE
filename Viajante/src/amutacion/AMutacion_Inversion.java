05eb73dd54630568ca8f6f10ac21dfb6536110a4
Pabs <pablomacveigh@gmail.com>
2016-04-19 02:52:44 +0200

    Arreglado parte de la mutación por inversión. Hay que adaptar las probabilidades.

--- Viajante/src/amutacion/AMutacion_Inversion.java
+++ Viajante/src/amutacion/AMutacion_Inversion.java
@@ -6,45 +6,47 @@ import implementation.Cromosoma;
 
 public class AMutacion_Inversion implements AMutacion {
 
-	// La mutación por inversión coje dos puntos de la cadena e invierte el orden de los elementos entre esos puntos.
+	// La mutaciï¿½n por inversiï¿½n coje dos puntos de la cadena e invierte el orden de los elementos entre esos puntos.
 	// abc|def|ghi => abc|fed|ghi
 	public void mutar(Cromosoma[] poblacion, float mutacionProb, Random rand) {
 
 		
-		//TODO Preguntar sobre la posibilidad de mutación, si hay varias mutaciones.
-		//TODO Preguntar si se trata de la inversión simple (invertir la cadena sobre el mismo sitio) o normal (invirtiendo la cadena
+		//TODO Preguntar sobre la posibilidad de mutaciï¿½n, si hay varias mutaciones.
+		//TODO Preguntar si se trata de la inversiï¿½n simple (invertir la cadena sobre el mismo sitio) o normal (invirtiendo la cadena
 		//y metiendola en un punto aleatorio)
 		
 		//para toda la poblacion
 		for(int i =0; i< poblacion.length; i++){
 			
 			int[] alelos = poblacion[i].getGenotipo();
-			int inicio = 0;
+			int inicio =1 ;
 			int fin;
 			
-			//Buscamos el primer punto de la inversión
-			while (rand.nextFloat() > mutacionProb && inicio<alelos.length){
+			//Buscamos el primer punto de la inversiï¿½n
+			while (rand.nextFloat() > mutacionProb && inicio<alelos.length-1){
 				inicio++;
 			}
-			
-			//Calculamos un segundo punto para la inversión.
-			//TODO evitar overflow
+			//Calculamos un segundo punto para la inversiï¿½n.
 			//TODO requiere evitar copiar madrid
 			fin = (int) Math.floor((alelos.length-inicio)*rand.nextFloat());
-
-			int[] copia = new int[fin-inicio];
-			System.arraycopy(alelos, inicio, copia, 0, fin-inicio);
-			//Invertimos el array
-			for(int j = 0; j < copia.length / 2; j++)
-			{
-			    int temp = copia[j];
-			    copia[j] = copia[copia.length - j - 1];
-			    copia[copia.length - j - 1] = temp;
+			fin+=inicio;
+			System.out.println("Inicio: "+inicio);
+			System.out.println("Fin: "+fin);
+			if (fin!=0){
+				int[] copia = new int[fin-inicio+1];
+				System.arraycopy(alelos, inicio, copia, 0, fin-inicio+1);
+				//Invertimos el array
+				for(int j = 0; j < copia.length / 2; j++)
+				{
+				    int temp = copia[j];
+				    copia[j] = copia[copia.length - j - 1];
+				    copia[copia.length - j - 1] = temp;
+				}
+				
+				//La devolvemos a su sitio.
+				System.arraycopy(copia, 0, alelos, inicio, fin-inicio+1);
+				poblacion[i].setGenList(alelos); //TODO utilizar otro mï¿½todo
 			}
-			
-			//La devolvemos a su sitio.
-			System.arraycopy(copia, 0, alelos, inicio, fin-inicio);
-			poblacion[i].setGenList(alelos); //TODO utilizar otro método
 		}
 		
 	}
