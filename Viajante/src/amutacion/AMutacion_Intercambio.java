28e236adaa9ad3e06506f6ea50dfeb82c276a90c
Jorge Sanchez <jorjatorz@gmail.com>
2016-04-24 17:29:36 +0200

    Corregido un error en la mutacion con intercambio

--- Viajante/src/amutacion/AMutacion_Intercambio.java
+++ Viajante/src/amutacion/AMutacion_Intercambio.java
@@ -13,20 +13,24 @@ public class AMutacion_Intercambio implements AMutacion {
 	@Override
 	public void mutar(Cromosoma[] poblacion, float mutacionProb, Random rand) {
 		
-		for (int i=0; i<poblacion.length; i++){
+		for (int i=0; i<poblacion.length; i++)
+		{
 			int alelos[] = poblacion[i].getGenotipo();
-			for (int j = 1; j<alelos.length; j++){
-				if (rand.nextFloat()<mutacionProb){
-					//Elegimos el elemento a intercambiar
-					int exchange = (int) (alelos.length-1 * rand.nextFloat()+1);
-					int aux = alelos[j];
+			//Recorremos todos los genes. Si mutan se intercambia el gen actual con otro aleatorio
+			for (int j = 1; j<alelos.length; j++)
+			{
+				if (rand.nextFloat()<mutacionProb)
+				{
+					//Elegimos el elemento a intercambiar [1-28]
+					int exchange = (int) ((alelos.length - 2) * rand.nextFloat()) + 1;
 					//Lo intercambiamos
+					int aux = alelos[j];
 					alelos[j]=alelos[exchange];
 					alelos[exchange]=aux;
-					//cambiamos el genotipo
-					poblacion[i].setGenotipo(alelos);
 				}
 			}
+			//cambiamos el genotipo
+			poblacion[i].setGenotipo(alelos);
 		}
 	}
 
